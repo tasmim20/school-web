754084920aad74755202059f81f3512f6059ade4
Tasmim Rahman <tasmimrahman2000@gmail.com>
2022-10-11 22:44:52 +0600

    toast added

--- src/components/Topic/Topic.js
+++ src/components/Topic/Topic.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import './Topic.css';
 import { Link } from 'react-router-dom';
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons'
 
 const Topic = ({topic}) => {
     const {name, logo, id} = topic;
@@ -11,7 +12,10 @@ const Topic = ({topic}) => {
            <div className='flex'>
            <h3 className='name'>{name}</h3>
            <Link to={`/topic/${id}`}>
-           <button className='button'>Start Practise</button></Link>
+           <button className='button'>
+        Start Practise
+           <FontAwesomeIcon className='icon' icon={faArrowRight}></FontAwesomeIcon>
+           </button></Link>
            </div>
             
         </div>
--- src/components/TopicDetail/TopicDetail.js
+++ src/components/TopicDetail/TopicDetail.js
@@ -1,16 +1,27 @@
 import React from 'react';
 import './TopicDetail.css';
 import Option from "../Option/Option"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faEye } from '@fortawesome/free-solid-svg-icons';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 
 const TopicDetail = ({topic}) => {
     const {question,options,correctAnswer} = topic;
 
+    const notify = () => toast(` ${correctAnswer}`);
    
     return (
         <div className='question'>
-            {/* <p><small>{correctAnswer}</small></p> */}
-            <h4><ol><li> Quiz.{question}</li></ol> </h4>
+          
+           <div className='answer'> 
+            <h4> Quiz.{question} </h4>
+            <button>
+                <FontAwesomeIcon  onClick={notify} icon={faEye}></FontAwesomeIcon>
+                <ToastContainer></ToastContainer>
+            </button>
+            </div>
             {options.map(option=> <Option key={option.id} option={option}></Option>)}
            
         </div>
