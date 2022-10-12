f1b020e91240cb2fb6d2af805495841d54dbda21
Tasmim Rahman <tasmimrahman2000@gmail.com>
2022-10-12 14:35:25 +0600

    question added

--- src/components/TopicDetail/TopicDetail.js
+++ src/components/TopicDetail/TopicDetail.js
@@ -1,28 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './TopicDetail.css';
-import Option from "../Option/Option"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {faEye } from '@fortawesome/free-solid-svg-icons';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { Button } from 'react-bootstrap';
 
 
-const TopicDetail = ({topic}) => {
-    const {question,options,correctAnswer} = topic;
 
-    const notify = () => toast(` ${correctAnswer}`);
+
+const TopicDetail = (props) => {
+    const {question,options,correctAnswer} = props.topic;
+
+    const [clicked, setClicked] = useState(false)
+
+    const notify = () => toast.warning(`Correct Answer: ${correctAnswer}`);
+
    
+
+    const selectOption = (option) =>{
+        if(correctAnswer === option){
+            toast.success('Well done, Your answer is Correct');
+            
+        }
+        else{
+            toast.error('Sorry..!! Incorrect answer');
+        }
+        setClicked(true);
+    }
+
+  
+
     return (
         <div className='question'>
           
            <div className='answer'> 
-            <h4> Quiz.{question} </h4>
+            <h4>{question} </h4>
             <button>
                 <FontAwesomeIcon  onClick={notify} icon={faEye}></FontAwesomeIcon>
-                <ToastContainer></ToastContainer>
+                <ToastContainer 
+        theme="colored"></ToastContainer>
             </button>
             </div>
-            {options.map(option=> <Option key={option.id} option={option}></Option>)}
+    {
+        options.map(option => <p className='flex2' onClick={()=> selectOption (option)}> <Button variant="light">{option}</Button> </p>)
+    }   
            
         </div>
     );
