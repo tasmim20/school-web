71208b06762f5a2b53dcf3fae8cd688af4ca1101
Tasmim Rahman <tasmimrahman2000@gmail.com>
2022-10-12 20:29:07 +0600

    chart added

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,66 +1,37 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { Bar, BarChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts';
 
-import React from "react";
-import {
-  BarChart,
-  Bar,
-  XAxis,
-  YAxis,
-  CartesianGrid,
-  Tooltip,
-  Legend
-} from "recharts";
+const Statistics = () => {
+    const [quizData, setQuizData] = useState([]);
+    useEffect(() => {
+        axios.get('https://openapi.programming-hero.com/api/quiz')
+            .then(data => {
+                const loadedData = data.data.data;
+                const singleData = loadedData.map(data => {
+                const parts = {
+                        total:data.total,
+                        name:data.name
+                    }
+                return parts;
+                });
+                setQuizData(singleData); 
+            })
+    }, []);
+    return (
+        <div className='bg-gray-400 py-20'>
+          <div className=' lg:w-2/4 lg:mx-auto bg-orange-400'>
+            <ResponsiveContainer width="90%" height={400}>
+            <BarChart width={200} height={400} data={quizData}>
+            <Bar dataKey="total" fill="#8884d8" />
+            <XAxis dataKey="name"></XAxis>
+            <Tooltip />
+            <YAxis></YAxis>
+            </BarChart>
+            </ResponsiveContainer>
+         </div>
+       </div>
+    );
+};
 
-const data = [
-  {
-    name: "React",
-    uv: 5,
-    pv: 10,
-    amt: 5
-  },
-  {
-    name: "JavaScript",
-    uv: 3000,
-    pv: 1398,
-    amt: 10
-  },
-  {
-    name: "CSS",
-    uv: 2000,
-    pv: 9800,
-    amt: 15
-  },
-  {
-    name: "GIT",
-    uv: 2780,
-    pv: 3908,
-    amt: 20
-  }
-
-];
-
-export default function App() {
-  return (
-   <div className="chart">
-     <BarChart
-      width={500}
-      height={400}
-      data={data}
-    
-      margin={{
-        top: 5,
-        right: 30,
-        left: 20,
-        bottom: 5
-      }}
-      barSize={20}
-    >
-      <XAxis dataKey="name" scale="point" padding={{ left: 10, right: 10 }} />
-      <YAxis dataKey="pv"/>
-      <Tooltip />
-      <Legend />
-      <CartesianGrid strokeDasharray="3 3" />
-      <Bar dataKey="pv" fill="#8884d8" background={{ fill: "#eee" }} />
-    </BarChart>
-   </div>
-  );
-}
+export default Statistics;
